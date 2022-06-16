1c318edac643ab1405b524256997ac2d7e68c03d
Kaze Wong <kazewong.physics@gmail.com>
2022-06-16 14:41:54 -0400

    two body seems working!?

---
+++ Autodiff_2body/two_body_problem.py
@@ -0,0 +1,54 @@
+from numpy import kaiser
+import jax
+import jax.numpy as np
+
+def f(x,t):
+    output = 0
+    for i in range(t):
+        output += x*i
+
+    return output
+
+def leapfrog_step(x, v, grad_potential, dt):
+    v_half = v + 0.5*grad_potential(x)*dt
+    x_new = x + v_half*dt
+    v_new = v_half + 0.5*grad_potential(x_new)*dt
+    return x_new, v_new
+
+def potential(x1, m1, m2, x2):
+    return m1*m2/np.sum((x1-x2)**2)
+
+grad_potential = jax.grad(potential)
+
+
+def simulator_step(m1, m2, x1, x2, v1, v2, dt=0.1):
+    v_half_1 = v1 + 0.5*grad_potential(x1, m1, m2, x2)*dt/m1
+    x_new_1 = x1 + v_half_1*dt
+    v_new_1 = v_half_1 + 0.5*grad_potential(x_new_1, m1, m2, x2)*dt/m1
+    v_half_2 = v2 + 0.5*grad_potential(x2, m2, m1, x1)*dt/m2
+    x_new_2 = x2 + v_half_2*dt
+    v_new_2 = v_half_2 + 0.5*grad_potential(x_new_2, m2, m1, x1)*dt/m2
+    return x_new_1, x_new_2, v_new_1, v_new_2
+
+def simulator(m1, m2, x1, x2, v1, v2, step, max_t):
+    dt = max_t/step
+    x2_array = []
+    for i in range(step):
+        x1, x2, v1, v2 = simulator_step(m1, m2, x1, x2, v1, v2, dt)
+        x2_array.append(x2)
+    return x2_array
+
+m1 = 100.
+m2 = 1.
+x1 = np.array([0.,0.,0.])
+x2 = np.array([10.,0.,0.])
+v1 = np.array([0.,0.,0.])
+v2 = np.array([0.,1.,0.])
+step = 100
+max_t = 10
+
+data = np.array(simulator(m1, m2, x1, x2, v1, v2, step, max_t))
+
+def loss(m1, m2, x1, x2, v1, v2, step, max_t):
+    x2_array = np.array(simulator(m1, m2, x1, x2, v1, v2, step, max_t))
+    return np.sum((x2_array-x2)**2)
--- MLP_prediction/AnnoyExperiment.py
+++ MLP_prediction/AnnoyExperiment.py
@@ -1,27 +1,32 @@
 import h5py
 from annoy import AnnoyIndex
+import numpy as np
 import random
 
-f = 40  # Length of item vector that will be indexed
+data = h5py.File("/mnt/home/apricewhelan/projects/gaia-scratch/data/gaiadr3-apogee-bprp-Xy.hdf5","r")
+
+X_ = data['X'][:]
+Y_ = data['y'][:]
+clean_index = np.where(np.invert(np.isnan(Y_[:,2])))[0]
+X_ = X_[clean_index]
+Y_ = Y_[clean_index]
+
+X_train = X_[:int(X_.shape[0]*0.8)]
+Y_train = Y_[:int(Y_.shape[0]*0.8)]
+X_test = X_[int(X_.shape[0]*0.8):]
+Y_test = Y_[int(Y_.shape[0]*0.8):]
+
+f = 23  # Length of item vector that will be indexed
 
 t = AnnoyIndex(f, 'angular')
-for i in range(1000):
-    v = [random.gauss(0, 1) for z in range(f)]
-    t.add_item(i, v)
+
+for i in range(X_train.shape[0]):
+    t.add_item(i, X_train[i])
 
 t.build(10) # 10 trees
 t.save('test.ann')
 
-# ...
-
-u = AnnoyIndex(f, 'angular')
-u.load('test.ann') # super fast, will just mmap the file
-print(u.get_nns_by_item(0, 1000)) # will find the 1000 nearest neighbors
-
-data = h5py.File("/mnt/home/apricewhelan/projects/gaia-scratch/data/gaiadr3-apogee-bprp-Xy.hdf5","r")
-
-t = AnnoyIndex(f, 'angular')
-X_ = data['X'][:]
-t.b
-
-Y_ = data['y'][:]
+pred_y = []
+for i in range(X_test.shape[0]):
+    index = t.get_nns_by_vector(X_test[i], 10)[1:]
+    pred_y.append(np.mean(Y_train[index],axis=0))
